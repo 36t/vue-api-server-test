ccddbf143ab1704a5dba3214f7c425583172b790
36t <saluta@gmail.com>
2021-03-05 10:26:02 +0900

    feat: added before method.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
+console.log("loaded vue.config.js");
+const path = require("path");
+const fs = require("fs-extra");
+
 module.exports = {
   devServer: {
     open: true,
     port: 3000,
-    disableHostCheck: true
+    disableHostCheck: true,
+    before: (app, server, compiler) => {
+      const apiPath = path.join(__dirname, "src", "api", "api.js");
+      const jsonPath = path.join(__dirname, "src", "api", "db.json");
+
+      console.log(apiPath);
+      console.log(jsonPath);
+    }
   }
 };
